0ce8cb8a16209c4979fe3d1bbf7962eef7d83472
kirandulawat <kirandulawat04@gmail.com>
2023-01-06 20:24:07 +0530

    login page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Login from './components/Login'
 import Signup from './components/Signup'
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/signup" element={<Signup/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
