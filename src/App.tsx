a56ca5cbfd76085f78c5b060c0ccfa6fd2e9ca79
rahulsuthar22 <rkcsuthar2@gmail.com>
2023-01-06 15:53:03 +0530

    initialize the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import Login from './components/Login'
+import Signup from './components/Signup'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/components/Signup.tsx
@@ -0,0 +1,11 @@
+
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Hiii this is sign up</h1>
+    </div>
+  )
+}
+
+export default Signup
