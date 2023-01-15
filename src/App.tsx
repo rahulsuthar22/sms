8ec226a5137b3a250096160342d41f6c5aa93f55
kirandulawat <kirandulawat04@gmail.com>
2023-01-15 14:05:44 +0530

    side navbar page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SideNavBar from './components/SideNavBar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/side-nav-bar" element={<SideNavBar />} />
         </Routes>
       </Router>
     </div>
